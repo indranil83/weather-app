eed5ef3e443b33228bc4516218b84e544ad8e871
indranil83 <pindranil83@gmail.com>
2024-08-27 16:17:21 +0530

    Weather

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Correct configuration for OpenWeatherMap API
+module.exports = {
+    key: "5bd01d4ec2945b7819ed702b2d1e4d3d",
+    base: "https://api.openweathermap.org/data/2.5/" // Base URL without specific parameters
+  };
+  
